3d6d605f1a305e5bb314056f8480ee74240436c9
jooaco9 <joaquin.corbo9@gmail.com>
2025-03-01 13:46:42 -0300

    comienzo de api

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+# Objeto app de tipo FastApi
+app = FastAPI()
+
+# Configuracion del ApiRestFul
+
+# Entrada GET http://localhost/
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
