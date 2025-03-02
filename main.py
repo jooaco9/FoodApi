46dcf2860395b3619f45e11ed42f0bfca48c7da2
jooaco9 <joaquin.corbo9@gmail.com>
2025-03-02 13:12:07 -0300

    agregado de configuracion para la documentacion

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 from fastapi import FastAPI
 
+# tag para poder agregar conjuntos en la documentacion
+tags_metadata = [
+    {
+        "name": "ingredientes",
+        "description": "Opercaiones relacionadas con el CRUD de ingredientes"
+    }
+]
+
 # Objeto app de tipo FastApi
-app = FastAPI()
+app = FastAPI(
+    title="FoodApi",
+    description="ApiRestFul para la gestion de alimentos y planes nutricionales",
+    version="0.0.2",
+    contact= {
+        "name": "Joaquin Corbo",
+        "url": "https://x.com/9_jokin",
+        "email": "joaquin.corbo9@gmail.com"
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html"
+    },
+    openapi_tags=tags_metadata
+)
 
 # Configuracion del ApiRestFul
 
-# Entrada GET http://localhost/
+# Endopoint GET /
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/ingredientes", tags=["ingredientes"])
+def read_ingredients():
+    return {"Objeto": "Ingredientes"}
+
