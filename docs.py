110f36e642094302fc5a9f7123f6f661321b7559
jooaco9 <joaquin.corbo9@gmail.com>
2025-03-04 09:53:32 -0300

    metadatos para el nuevo conjuto de endpoints: platos

--- docs.py
+++ docs.py
@@ -3,5 +3,9 @@ tags_metadata = [
     {
         "name": "ingredientes",
         "description": "Opercaiones relacionadas con el CRUD de ingredientes"
+    },
+    {
+        "name": "platos",
+        "description": "Operaciones relacionadas con el CRUD de platos",
     }
 ]
